ffb99008bb059f7a75608ecb5c425d9a03b4707b
Nikita Zaitsev <nikita.zaitsev@electroneek.com>
2020-01-24 18:55:15 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header.jsx';
+import Navbar from './components/Navbar.jsx';
+import Profile from './components/Profile.jsx';
+
+const App = () => {
+    return (
+        <div className='app-wrapper'>
+            <Header />
+            <Navbar />
+            <Profile />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src="https://cache3.youla.io/files/images/780_780/59/71/5971d97a66fb0768f17dd8c4.jpg" />
+    </header>
+}
+
+export default Header
